003f889a517524e7433feeaee31033367561751d
Vitor M. A. da Cruz <vitor.mazzi@intelie.com.br>
2019-05-31 20:39:37 -0300

    Detecting commingled and focused flows, sampling and seal losses

--- src/main/process_modules/sampling_monitor.py
+++ src/main/process_modules/sampling_monitor.py
@@ -143,8 +143,13 @@ def find_rate_change(process_name, probe_name, probe_data, event_list, message_s
     index_mnemonic = probe_data['index_mnemonic']
     flow_rate_mnemonic = probe_data['pumpout_flowrate_mnemonic']
     probe_state = probe_data.get('process_state', PUMP_STATES.INACTIVE)
+
     pump_activation_timestamp = probe_data.get('pump_activation_timestamp', 0)
+    pump_activation_index = probe_data.get('pump_activation_index', 0)
     pump_deactivation_timestamp = probe_data.get('pump_deactivation_timestamp', 0)
+    pump_deactivation_index = probe_data.get('pump_deactivation_index', 0)
+    rate_change_timestamp = probe_data.get('rate_change_timestamp', 0)
+    rate_change_index = probe_data.get('rate_change_index', 0)
 
     # In order to avoid detecting the same event twice we must trim the set of events
     # We also must ignore events without data
@@ -157,11 +162,13 @@ def find_rate_change(process_name, probe_name, probe_data, event_list, message_s
     )
 
     # Check whether, {flow_rate_mnemonic} has changed
-    fluctuation_tolerance = 1
     latest_flowrate = probe_data.get('latest_flowrate', 0)
     latest_motorspeed = probe_data.get('latest_motorspeed', 0)
-    low_cut = latest_flowrate - fluctuation_tolerance
+
+    fluctuation_tolerance = 1
+
     high_cut = latest_flowrate + fluctuation_tolerance
+    low_cut = latest_flowrate - fluctuation_tolerance
 
     logging.debug("Probe {}: Ignoring flow rates between {:.2f} and {:.2f}".format(
         probe_name, low_cut, high_cut
@@ -178,7 +185,7 @@ def find_rate_change(process_name, probe_name, probe_data, event_list, message_s
     if has_rate_change:
         depth_mnemonic = probe_data['depth_mnemonic']
         pressure_mnemonic = probe_data['pressure_mnemonic']
-        motor_speed_mnemonic = probe_data['pump_motor_speed']
+        motor_speed_mnemonic = probe_data['pump_motor_speed_mnemonic']
 
         # The rate started changing at the first of these events
         # Find out when the rate stabilized, based on the last {flow_rate_mnemonic}
@@ -221,29 +228,31 @@ def find_rate_change(process_name, probe_name, probe_data, event_list, message_s
             latest_motorspeed, motor_speed,
             pressure
         )
-        message_sender(process_name, message, timestamp=event_timestamp)
+        message_sender(
+            process_name,
+            message,
+            timestamp=event_timestamp,
+            message_type=messenger.MESSAGE_TYPES.CHAT
+        )
 
         if (flow_rate < 1):
             detected_state = PUMP_STATES.BUILDUP_EXPECTED
-
-            if (motor_speed > 0):
-                # Flow is almost stopping, pumps should also be stopped
-                message = "*Alarm, probable seal loss!* \nMotor speed and flow rate diverging for probe {}@{:.2f} ft.".format(
-                    probe_name, depth
-                )
-                message_sender(process_name, message, timestamp=event_timestamp)
         else:
             detected_state = PUMP_STATES.PUMPING
 
         if probe_state != detected_state:
             if detected_state == PUMP_STATES.PUMPING:
                 pump_activation_timestamp = event_timestamp
+                pump_activation_index = etim
             else:
                 pump_deactivation_timestamp = event_timestamp
+                pump_deactivation_index = etim
 
         latest_flowrate = flow_rate
         latest_motorspeed = motor_speed
         latest_seen_index = etim
+        rate_change_timestamp = event_timestamp
+        rate_change_index = etim
         logging.debug(message)
     else:
         detected_state = None
@@ -253,13 +262,326 @@ def find_rate_change(process_name, probe_name, probe_data, event_list, message_s
         latest_flowrate=latest_flowrate,
         latest_motorspeed=latest_motorspeed,
         pump_activation_timestamp=pump_activation_timestamp,
+        pump_activation_index=pump_activation_index,
         pump_deactivation_timestamp=pump_deactivation_timestamp,
+        pump_deactivation_index=pump_deactivation_index,
+        rate_change_timestamp=rate_change_timestamp,
+        rate_change_index=rate_change_index,
     )
     return detected_state
 
 
+def check_seal_health(process_name, probe_name, probe_data, event_list, message_sender=None):
+    index_mnemonic = probe_data['index_mnemonic']
+    flow_rate_mnemonic = probe_data['pumpout_flowrate_mnemonic']
+    motor_speed_mnemonic = probe_data['pump_motor_speed_mnemonic']
+    depth_mnemonic = probe_data['depth_mnemonic']
+    buildup_duration = probe_data['buildup_duration']
+
+    has_seal = probe_data.get('has_seal', True)
+
+    # In order to avoid detecting the same event twice we must trim the set of events
+    # We also must ignore events without data
+    latest_seen_index = probe_data.get('latest_seen_index', 0)
+    valid_events = loop.filter_events(
+        event_list,
+        latest_seen_index,
+        index_mnemonic,
+        flow_rate_mnemonic
+    )
+
+    # Check whether pump is active but there is no flow or vice-versa
+    if valid_events:
+        last_event = valid_events[-1]
+        last_flowrate = last_event.get(flow_rate_mnemonic)
+        last_motor_speed = last_event.get(motor_speed_mnemonic)
+        last_index = last_event.get(index_mnemonic)
+
+        is_stopped = (last_motor_speed == last_flowrate == 0)
+        is_running = (last_motor_speed > 0) and (last_flowrate > 0)
+
+        if has_seal and (not (is_stopped or is_running)):
+            # We found an inconsistency, find out where it started
+            if last_motor_speed > 0:
+                # When did the flow stop?
+                target_mnemonic = flow_rate_mnemonic
+            else:
+                # When did the pump stop?
+                target_mnemonic = motor_speed_mnemonic
+
+            # Drop all events where {target_mnemonic} > 0
+            events_before_stopping = list(
+                dropwhile(
+                    lambda event: event.get(target_mnemonic) > 0,
+                    valid_events
+                )
+            )
+
+            # The inconsistency started in the first of these events
+            reference_event = events_before_stopping[0]
+
+            # If the inconsistency happened less than {buildup_duration} ago, ignore it
+            etim = reference_event.get(index_mnemonic, -1)
+            if (last_index - etim) >= buildup_duration:
+                depth = reference_event.get(depth_mnemonic, -1)
+                motor_speed = reference_event.get(motor_speed_mnemonic, -1)
+                flow_rate = reference_event.get(flow_rate_mnemonic, -1)
+                event_timestamp = reference_event.get('timestamp', timestamp.get_timestamp())
+
+                # Send a message about the seal loss
+                message = (
+                    "*Alarm, probable seal loss for probe {}@{:.0f} ft at {:.0f}!*"
+                    "\nMotor speed {:.2f} rpm and flow rate {:.2f} cm³/s!"
+                ).format(
+                    probe_name, depth, etim, motor_speed, flow_rate
+                )
+                message_sender(process_name, message, timestamp=event_timestamp)
+
+                has_seal = False
+                latest_seen_index = etim
+
+    probe_data.update(
+        has_seal=has_seal,
+        latest_seen_index=latest_seen_index,
+    )
 
 
+def find_commingled_flow(process_name, process_settings, event_list, message_sender=None):
+    # When both pumps are started we get to commingled flow
+    commingled_flow_start_ts = process_settings.get('commingled_flow_start_ts', 0)
+    commingled_flow_end_ts = process_settings.get('commingled_flow_start_ts', 0)
+    monitor_settings = process_settings['monitor']
+
+    probes = monitor_settings.get('probes', [])
+    pumping_probes = [
+        item.get('process_state', PUMP_STATES.INACTIVE) == PUMP_STATES.PUMPING
+        for item in probes.values()
+    ]
+
+    detected_state = None
+    if all(pumping_probes):
+        pump_activations_timestamps = [
+            item.get('pump_activation_timestamp', -1)
+            for item in probes.values()
+        ]
+        pump_activations_indexes = [
+            item.get('pump_activation_index', -1)
+            for item in probes.values()
+        ]
+
+        # If the pumps are activated more than 30 seconds apart from each other, ignore them
+        first_activation_timestamp = min(pump_activations_timestamps)
+        first_activation_index = min(pump_activations_indexes)
+        last_activation_index = max(pump_activations_indexes)
+        if (last_activation_index - first_activation_index) < 30:
+            commingled_flow_start_ts = first_activation_timestamp
+            detected_state = SAMPLING_STATES.COMMINGLED_FLOW
+
+            message = "Commingled flow started at {:.0f}s.".format(first_activation_index)
+            message_sender(process_name, message, timestamp=commingled_flow_start_ts)
+            logging.info(message)
+
+    process_settings.update(
+        latest_seen_index=commingled_flow_start_ts,
+        commingled_flow_start_ts=commingled_flow_start_ts,
+        commingled_flow_end_ts=commingled_flow_end_ts,
+    )
+    return detected_state
+
+
+def find_focused_flow(process_name, process_settings, event_list, message_sender=None):
+    # When rates of both pumps are stable for more than {focused_flow_grace_period}
+    monitor_settings = process_settings['monitor']
+    index_mnemonic = monitor_settings['index_mnemonic']
+    focused_flow_grace_period = monitor_settings.get('focused_flow_grace_period', 60)
+
+    commingled_flow_end_ts = process_settings.get('commingled_flow_end_ts', 0)
+    focused_flow_start_ts = process_settings.get('focused_flow_start_ts', 0)
+    focused_flow_end_ts = process_settings.get('focused_flow_start_ts', 0)
+    focused_flow_start_index = process_settings.get('focused_flow_start_index', 0)
+
+    probes = monitor_settings.get('probes', [])
+    pumping_probes = [
+        item.get('process_state', PUMP_STATES.INACTIVE) == PUMP_STATES.PUMPING
+        for item in probes.values()
+    ]
+
+    detected_state = None
+    if all(pumping_probes):
+        rate_changes_timestamps = [
+            item.get('rate_change_timestamp', -1)
+            for item in probes.values()
+        ]
+        rate_changes_indexes = [
+            item.get('rate_change_index', -1)
+            for item in probes.values()
+        ]
+        flowrates = [item.get('latest_flowrate', 0) for item in probes.values()]
+        motor_speeds = [item.get('latest_motorspeed', 0) for item in probes.values()]
+
+        latest_index = event_list[-1].get(index_mnemonic, 0)
+        last_activation_index = max(rate_changes_indexes)
+        current_flow_duration = latest_index - last_activation_index
+
+        if current_flow_duration >= focused_flow_grace_period:
+            last_activation_ts = max(rate_changes_timestamps)
+            focused_flow_start_ts = last_activation_ts
+            focused_flow_start_index = last_activation_index
+            commingled_flow_end_ts = last_activation_ts
+            detected_state = SAMPLING_STATES.FOCUSED_FLOW
+
+            message = (
+                "Focused flow started at {:.1f}s."
+                "\n   Flow rates: {:.2f} and {:.2f} cm³/s (ratio: {:.3f})"
+                "\n   Motor speeds: {:.2f} and {:.2f} rpm (ratio: {:.3f})"
+            ).format(
+                last_activation_index,
+                flowrates[0], flowrates[1], (flowrates[0] / flowrates[1]),
+                motor_speeds[0], motor_speeds[1], (motor_speeds[0] / motor_speeds[1]),
+            )
+            message_sender(process_name, message, timestamp=focused_flow_start_ts)
+            logging.info(message)
+    else:
+        # COMMINGLED_FLOW stopped
+        rate_changes_timestamps = [
+            item.get('rate_change_timestamp', -1)
+            for item in probes.values()
+        ]
+        rate_changes_indexes = [
+            item.get('rate_change_index', -1)
+            for item in probes.values()
+        ]
+
+        last_activation_index = max(rate_changes_indexes)
+        commingled_flow_end_ts = max(rate_changes_timestamps)
+
+        detected_state = SAMPLING_STATES.INACTIVE
+
+    process_settings.update(
+        latest_seen_index=focused_flow_start_index,
+        commingled_flow_end_ts=commingled_flow_end_ts,
+        focused_flow_start_ts=focused_flow_start_ts,
+        focused_flow_end_ts=focused_flow_end_ts,
+    )
+    return detected_state
+
+
+def find_sampling_start(process_name, process_settings, event_list, message_sender=None):
+    # A pump will be stopped in order to collect a sample
+    monitor_settings = process_settings['monitor']
+    sampling_start_index = process_settings.get('sampling_start_index', 0)
+    sampling_start_ts = process_settings.get('sampling_start_ts', 0)
+    sampling_end_index = process_settings.get('sampling_end_index', 0)
+    sampling_end_ts = process_settings.get('sampling_end_ts', 0)
+
+    detected_state = None
+    pumping_probes = []
+    idle_probes = []
+    probes = monitor_settings.get('probes', [])
+
+    for probe_name, probe_data in probes.items():
+        probe_state = probe_data.get('process_state', PUMP_STATES.INACTIVE)
+        if probe_state == PUMP_STATES.PUMPING:
+            pumping_probes.append(probe_name)
+        else:
+            idle_probes.append(probe_name)
+
+    # The guard probe should be active and the sampling probe should be idle
+    if idle_probes and pumping_probes:
+        sampling_probe = idle_probes[0]
+        sampling_probe_data = probes[sampling_probe]
+
+        # Find out if the pump is running
+        index_mnemonic = sampling_probe_data['index_mnemonic']
+        motor_speed_mnemonic = sampling_probe_data['pump_motor_speed_mnemonic']
+        latest_seen_index = sampling_probe_data.get('latest_seen_index', 0)
+        valid_events = loop.filter_events(
+            event_list,
+            latest_seen_index,
+            index_mnemonic,
+            motor_speed_mnemonic,
+        )
+
+        if valid_events:
+            last_motor_speed = valid_events[-1].get(motor_speed_mnemonic)
+        else:
+            last_motor_speed = 0
+
+        if last_motor_speed == 0:
+            rate_change_timestamp = sampling_probe_data.get('rate_change_timestamp', -1)
+            rate_change_index = sampling_probe_data.get('rate_change_index', -1)
+
+            sampling_start_ts = rate_change_timestamp
+            sampling_start_index = rate_change_index
+
+            message = "Sample collection started at {:.1f}s.".format(
+                sampling_start_index,
+            )
+            message_sender(process_name, message, timestamp=sampling_start_ts)
+            logging.info(message)
+            detected_state = SAMPLING_STATES.SAMPLING
+
+    elif not idle_probes:
+        detected_state = SAMPLING_STATES.FOCUSED_FLOW
+
+    process_settings.update(
+        latest_seen_index=sampling_start_index,
+        sampling_start_index=sampling_start_index,
+        sampling_start_ts=sampling_start_ts,
+        sampling_end_index=sampling_end_index,
+        sampling_end_ts=sampling_end_ts,
+    )
+    return detected_state
+
+
+def find_sampling_end(process_name, process_settings, event_list, message_sender=None):
+    # When the second pump gets reactivated
+    monitor_settings = process_settings['monitor']
+    sampling_start_index = process_settings.get('sampling_start_index', 0)
+    sampling_start_ts = process_settings.get('sampling_start_ts', 0)
+    sampling_end_index = process_settings.get('sampling_end_index', 0)
+    sampling_end_ts = process_settings.get('sampling_end_ts', 0)
+
+    probes = monitor_settings.get('probes', [])
+    pumping_probes = [
+        item.get('process_state', PUMP_STATES.INACTIVE) == PUMP_STATES.PUMPING
+        for item in probes.values()
+    ]
+
+    detected_state = None
+    if all(pumping_probes):
+        rate_changes_timestamps = [
+            item.get('rate_change_timestamp', -1)
+            for item in probes.values()
+        ]
+        rate_changes_indexes = [
+            item.get('rate_change_index', -1)
+            for item in probes.values()
+        ]
+
+        last_activation_index = max(rate_changes_indexes)
+        last_activation_ts = max(rate_changes_timestamps)
+
+        sampling_end_ts = last_activation_ts
+        sampling_end_index = last_activation_index
+        detected_state = SAMPLING_STATES.FOCUSED_FLOW
+
+        message = (
+            "Sample collection finished at {:.1f}s."
+            "\n  Sampling process took {:.1f}s"
+        ).format(
+            sampling_end_index, (sampling_end_index - sampling_start_index)
+        )
+        message_sender(process_name, message, timestamp=sampling_end_ts)
+        logging.info(message)
+
+    process_settings.update(
+        latest_seen_index=sampling_start_index,
+        sampling_start_ts=sampling_start_ts,
+        sampling_end_index=sampling_end_index,
+        sampling_end_ts=sampling_end_ts,
+    )
+    return detected_state
 
 
 def run_probe_monitor(process_name, probe_name, probe_data, event_list, functions_map):
@@ -274,6 +596,13 @@ def run_probe_monitor(process_name, probe_name, probe_data, event_list, function
 
     state_transition_func = pump_functions[probe_state]
     detected_state = state_transition_func(
+        process_name,
+        probe_name,
+        probe_data,
+        event_list,
+        message_sender=message_func,
+    )
+    check_seal_health(
         process_name,
         probe_name,
         probe_data,
@@ -310,6 +639,7 @@ def run_sampling_monitor(process_name, process_settings, event_list, functions_m
     sampling_transition_func = sampling_functions[sampling_state]
     detected_state = sampling_transition_func(
         process_name,
+        process_settings,
         event_list,
         message_sender=message_func,
     )
@@ -344,11 +674,13 @@ def run_monitor(process_name, process_settings, event_list, functions_map):
             buildup_duration=buildup_duration,
             buildup_wait_period=buildup_wait_period,
         )
+        probe_data = loop.maybe_reset_latest_index(probe_data, event_list)
         run_probe_monitor(process_name, probe_name, probe_data, event_list, functions_map)
 
     # Refresh the state for the sampling process
-    # @@@
-    # sampling_state = run_sampling_monitor(process_name, prmat(process_name))
+    probe_data = loop.maybe_reset_latest_index(process_settings, event_list)
+    sampling_state = run_sampling_monitor(process_name, process_settings, event_list, functions_map)
+    return sampling_state
 
 
 def start(process_name, process_settings, output_info, _settings):
@@ -374,6 +706,12 @@ def start(process_name, process_settings, output_info, _settings):
                 },
                 fallback_state=PUMP_STATES.INACTIVE,
             ),
+        },
+        'sampling': {
+            SAMPLING_STATES.INACTIVE: find_commingled_flow,
+            SAMPLING_STATES.COMMINGLED_FLOW: find_focused_flow,
+            SAMPLING_STATES.FOCUSED_FLOW: find_sampling_start,
+            SAMPLING_STATES.SAMPLING: find_sampling_end,
         },
         'send_message': partial(
             messenger.send_message,
