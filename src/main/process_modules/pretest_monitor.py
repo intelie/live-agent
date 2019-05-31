223ed8b12fdd5889e8380fef449aa5a6779cc8cd
Vitor M. A. da Cruz <vitor.mazzi@intelie.com.br>
2019-05-31 11:23:44 -0300

    Extracing the stable buildup detection function
    The function ended up being exactly the same on both monitors

--- src/main/process_modules/pretest_monitor.py
+++ src/main/process_modules/pretest_monitor.py
@@ -207,119 +207,6 @@ def find_buildup(process_name, probe_name, probe_data, event_list, message_sende
     return detected_state
 
 
-def find_stable_buildup(process_name, probe_name, probe_data, event_list, message_sender, targets=None):
-    """
-    State when the slope of the linear regression of {pressure_mnemonic}
-    over {buildup_duration} seconds is <= {target_slope}
-    """
-    index_mnemonic = probe_data['index_mnemonic']
-    pressure_mnemonic = probe_data['pressure_mnemonic']
-    depth_mnemonic = probe_data['depth_mnemonic']
-    buildup_duration = probe_data['buildup_duration']
-    buildup_wait_period = probe_data['buildup_wait_period']
-    target_slopes = sorted(targets)
-    detected_state = None
-
-    # In order to avoid detecting the same event twice we must trim the set of events
-    # We also must ignore events without data
-    latest_seen_index = probe_data.get('latest_seen_index', 0)
-    valid_events = loop.filter_events(
-        event_list,
-        latest_seen_index,
-        index_mnemonic,
-        pressure_mnemonic
-    )
-
-    logging.debug("{}: Trying to detect a buildup with a slope <= {}, watching {} events".format(
-        process_name,
-        ', '.join(str(item) for item in target_slopes),
-        len(valid_events)
-    ))
-
-    data = [
-        {
-            timestamp: item.get('timestamp'),
-            index_mnemonic: item.get(index_mnemonic),
-            pressure_mnemonic: item.get(pressure_mnemonic),
-            depth_mnemonic: item.get(depth_mnemonic),
-        }
-        for item in valid_events
-    ]
-
-    regression_results = monitors.find_slope(
-        process_name,
-        data,
-        index_mnemonic,
-        pressure_mnemonic,
-        targets=target_slopes,
-        target_r=0.5,
-        window_size=buildup_duration
-    )
-
-    segment_found = regression_results.get('segment')
-    pretest_end_timestamp = None
-    target_state = None
-
-    if segment_found:
-        segment_slope = regression_results.get('segment_slope')
-        r_score = regression_results.get('r_score')
-        target_slope = regression_results.get('target_slope')
-        target_state = targets[target_slope]
-
-        # Use the last event of the segment as reference
-        reference_event = segment_found[-1]
-        etim = reference_event.get(index_mnemonic, -1)
-        pressure = reference_event.get(pressure_mnemonic, -1)
-        depth = reference_event.get(depth_mnemonic, -1)
-        pretest_end_timestamp = reference_event.get('timestamp', timestamp.get_timestamp())
-
-        message = "Probe {}@{:.0f} ft: Buildup stabilized within {} ({:.3f}, r²: {:.3f}) at {:.2f} s with pressure {:.2f} psi"  # NOQA
-        message_sender(
-            process_name,
-            message.format(
-                probe_name,
-                depth,
-                target_slope,
-                segment_slope,
-                r_score,
-                etim,
-                pressure
-            ),
-            timestamp=pretest_end_timestamp
-        )
-
-        detected_state = target_state
-        latest_seen_index = etim
-        logging.debug("Probe {}: Pretest finished at {:.0f}".format(probe_name, pretest_end_timestamp))
-
-    else:
-        measured_slopes = regression_results.get('measured_slopes', [])
-        logging.debug("{}: Buildup did not stabilize within {}. Measured slopes were: {}".format(
-            process_name, max(target_slopes), measured_slopes
-        ))
-
-        # If a stable buildup takes too long, give up
-        latest_event_index = data[-1].get(index_mnemonic)
-        wait_period = latest_event_index - latest_seen_index
-        depth = data[-1].get(depth_mnemonic, -1)
-        if wait_period > buildup_wait_period:
-            message = "Probe {}@{:.0f} ft: Buildup did not stabilize within {} after {} s"  # NOQA
-            message_sender(
-                process_name,
-                message.format(probe_name, depth, target_slope, wait_period),
-                timestamp=reference_event.get('timestamp')
-            )
-
-            detected_state = PRETEST_STATES.INACTIVE
-            latest_seen_index = latest_event_index
-
-    probe_data.update(
-        latest_seen_index=latest_seen_index,
-        pretest_end_timestamp=pretest_end_timestamp,
-    )
-    return detected_state
-
-
 def find_pump_recycle(process_name, probe_name, probe_data, event_list, message_sender):
     """State when {pretest_volume_mnemonic} returns to zero"""
     index_mnemonic = probe_data['index_mnemonic']
@@ -423,17 +310,19 @@ def start(process_name, process_settings, output_info, _settings):
         PRETEST_STATES.INACTIVE: find_drawdown,
         PRETEST_STATES.DRAWDOWN_START: find_buildup,
         PRETEST_STATES.DRAWDOWN_END: partial(
-            find_stable_buildup,
+            monitors.find_stable_buildup,
             targets={
                 0.01: PRETEST_STATES.INACTIVE,
                 0.1: PRETEST_STATES.BUILDUP_STABLE,
-            }
+            },
+            fallback_state=PRETEST_STATES.INACTIVE,
         ),
         PRETEST_STATES.BUILDUP_STABLE: partial(
-            find_stable_buildup,
+            monitors.find_stable_buildup,
             targets={
                 0.01: PRETEST_STATES.INACTIVE,
-            }
+            },
+            fallback_state=PRETEST_STATES.INACTIVE,
         ),
         'send_message': partial(
             messenger.send_message,
--- src/main/process_modules/sampling_monitor.py
+++ src/main/process_modules/sampling_monitor.py
@@ -222,115 +222,8 @@ def find_rate_change(process_name, probe_name, probe_data, event_list, message_s
     return detected_state
 
 
-def find_stable_buildup(process_name, probe_name, probe_data, event_list, message_sender, targets=None):
-    """
-    State when the slope of the linear regression of {pressure_mnemonic}
-    over {buildup_duration} seconds is <= {target_slope}
-    """
-    index_mnemonic = probe_data['index_mnemonic']
-    pressure_mnemonic = probe_data['pressure_mnemonic']
-    depth_mnemonic = probe_data['depth_mnemonic']
-    buildup_duration = probe_data['buildup_duration']
-    buildup_wait_period = probe_data['buildup_wait_period']
-    target_slopes = sorted(targets)
-    detected_state = None
 
-    # In order to avoid detecting the same event twice we must trim the set of events
-    # so we avoid looking into the same events twice
-    # We also must ignore events without data
-    latest_seen_index = probe_data.get('latest_seen_index', 0)
-    valid_events = loop.filter_events(
-        event_list,
-        latest_seen_index,
-        index_mnemonic,
-        pressure_mnemonic
-    )
 
-    logging.debug("{}: Trying to detect a buildup with a slope <= {}, watching {} events".format(
-        process_name,
-        ', '.join(str(item) for item in target_slopes),
-        len(valid_events)
-    ))
-
-    data = [
-        {
-            timestamp: item.get('timestamp'),
-            index_mnemonic: item.get(index_mnemonic),
-            pressure_mnemonic: item.get(pressure_mnemonic),
-            depth_mnemonic: item.get(depth_mnemonic),
-        }
-        for item in valid_events
-    ]
-
-    regression_results = monitors.find_slope(
-        process_name,
-        data,
-        index_mnemonic,
-        pressure_mnemonic,
-        targets=target_slopes,
-        target_r=0.5,
-        window_size=buildup_duration
-    )
-
-    segment_found = regression_results.get('segment')
-    buildup_end_timestamp = None
-    target_state = None
-
-    if segment_found:
-        segment_slope = regression_results.get('segment_slope')
-        r_score = regression_results.get('r_score')
-        target_slope = regression_results.get('target_slope')
-        target_state = targets[target_slope]
-
-        # Use the last event of the segment as reference
-        reference_event = segment_found[-1]
-        etim = reference_event.get(index_mnemonic, -1)
-        pressure = reference_event.get(pressure_mnemonic, -1)
-        depth = reference_event.get(depth_mnemonic, -1)
-        buildup_end_timestamp = reference_event.get('timestamp', timestamp.get_timestamp())
-
-        message = (
-            "Probe {}@{:.0f} ft: Buildup stabilized within {} ({:.3f}, r²: {:.3f}) at {:.2f} s with pressure {:.2f} psi"  # NOQA
-        ).format(
-            probe_name,
-            depth,
-            target_slope,
-            segment_slope,
-            r_score,
-            etim,
-            pressure
-        )
-        message_sender(process_name, message, timestamp=buildup_end_timestamp)
-
-        detected_state = target_state
-        latest_seen_index = etim
-        logging.debug(message)
-
-    else:
-        measured_slopes = regression_results.get('measured_slopes', [])
-        logging.debug("{}: Buildup did not stabilize within {}. Measured slopes were: {}".format(
-            process_name, max(target_slopes), measured_slopes
-        ))
-
-        # If a stable buildup takes too long, give up
-        latest_event_index = data[-1].get(index_mnemonic)
-        wait_period = latest_event_index - latest_seen_index
-        if wait_period > buildup_wait_period:
-            message = "Probe {}@{:.0f} ft: Buildup did not stabilize within {} after {} s"  # NOQA
-            message_sender(
-                process_name,
-                message.format(probe_name, depth, target_slope, wait_period),
-                timestamp=reference_event.get('timestamp')
-            )
-
-            detected_state = PUMP_STATES.INACTIVE
-            latest_seen_index = latest_event_index
-
-    probe_data.update(
-        latest_seen_index=latest_seen_index,
-        buildup_end_timestamp=buildup_end_timestamp,
-    )
-    return detected_state
 
 
 def run_probe_monitor(process_name, probe_name, probe_data, event_list, functions_map):
@@ -431,17 +324,19 @@ def start(process_name, process_settings, output_info, _settings):
             PUMP_STATES.INACTIVE: find_rate_change,
             PUMP_STATES.PUMPING: find_rate_change,
             PUMP_STATES.BUILDUP_EXPECTED: partial(
-                find_stable_buildup,
+                monitors.find_stable_buildup,
                 targets={
                     0.01: PUMP_STATES.INACTIVE,
                     0.1: PUMP_STATES.BUILDUP_STABLE,
-                }
+                },
+                fallback_state=PUMP_STATES.INACTIVE,
             ),
             PUMP_STATES.BUILDUP_STABLE: partial(
-                find_stable_buildup,
+                monitors.find_stable_buildup,
                 targets={
                     0.01: PUMP_STATES.INACTIVE,
-                }
+                },
+                fallback_state=PUMP_STATES.INACTIVE,
             ),
         },
         'send_message': partial(
