16cfc97b1701ed973b386f7099a5a9efeb3dc243
Vitor M. A. da Cruz <vitor.mazzi@intelie.com.br>
2019-10-01 18:17:21 -0300

    Flowrate monitor: Logic moved to the query run by live

--- src/main/process_modules/flowrate_monitor.py
+++ src/main/process_modules/flowrate_monitor.py
@@ -1,110 +1,125 @@
 # -*- coding: utf-8 -*-
 from functools import partial
-import requests
+import queue
 from setproctitle import setproctitle
-from eliot import Action
+from eliot import Action, start_action
 
 from live_client.utils import logging
-from utils import loop, monitors
+from utils import monitors
 
 
 __all__ = [
     'start'
 ]
 
+read_timeout = 120
+request_timeout = (3.05, 5)
+max_retries = 5
 
-def check_rate(process_name, flowrate_data, accumulator, process_settings, send_message):
-    monitor_settings = process_settings.get('monitor', {})
-    index_mnemonic = monitor_settings['index_mnemonic']
-    window_duration = monitor_settings['window_duration']
 
-    # Calculate the number of changes per mnemonic during the window
-    accumulator, begin, end = loop.refresh_accumulator(
-        flowrate_data, accumulator, index_mnemonic, window_duration
-    )
-    flowrate_mnemonics = monitor_settings['flowrate_mnemonics']
-    change_counter = dict(
-        (mnemonic, set())
-        for mnemonic in flowrate_mnemonics
-    )
-    latest_changes = dict(
-        (mnemonic, 0)
-        for mnemonic in flowrate_mnemonics
-    )
+def check_rate(process_name, accumulator, settings, send_message):
+    if not accumulator:
+        return
 
-    for event in accumulator:
-        for mnemonic in flowrate_mnemonics:
-            index = event.get(index_mnemonic)
-            mnemonic_value = event.get(mnemonic)
-            if mnemonic_value:
-                change_counter[mnemonic].add("{:.1f}".format(mnemonic_value))
-                latest_changes[mnemonic] = index
+    monitor_settings = settings.get('monitor', {})
+    flowrate_mnemonic = monitor_settings['flowrate_mnemonic']
+    latest_event = accumulator[-1]
 
     # Generate alerts whether the threshold was reached
-    # and the most recent change was in the latest batch of events
-    template = u'Whoa! {} was changed {} times over the last {} seconds, please calm down ({})'
-
-    max_threshold = monitor_settings['max_threshold']
-    interval = process_settings['request']['interval']
-    interval_start = end - interval
-
-    for mnemonic in flowrate_mnemonics:
-        number_of_changes = len(change_counter[mnemonic])
-        latest_change = latest_changes[mnemonic]
-        if (number_of_changes > max_threshold) and (latest_change > interval_start):
-            message = template.format(
-                mnemonic,
-                number_of_changes,
-                int(end - begin),
-                ', '.join(change_counter[mnemonic])
-            )
-            send_message(process_settings, message)
+    # a new event means another threshold breach
+    interval = (latest_event['end'] - latest_event['start']) / 1000
+    template = (
+        u'Whoa! {} was changed {} times over the last {} seconds, please calm down ({})'
+    )
+    message = template.format(
+        flowrate_mnemonic,
+        latest_event['num_changes'],
+        interval,
+        latest_event['values_list'],
+    )
+    send_message(settings, message)
 
     return accumulator
 
 
-def start(process_name, process_settings, helpers=None, task_id=None):
-    with Action.continue_task(task_id=task_id):
+def build_query(settings):
+    event_type = settings.get('event_type')
+    monitor_settings = settings.get('monitor', {})
+    flowrate_mnemonic = monitor_settings['flowrate_mnemonic']
+    max_threshold = monitor_settings['max_threshold']
+    window_duration = monitor_settings['window_duration']
+    sampling_frequency = monitor_settings['sampling_frequency']
+    precision = monitor_settings['precision']
+
+    query = f"""
+        {event_type} mnemonic!:{flowrate_mnemonic}
+        => value as current_value,
+           value#:round({precision}):dcount() as num_changes,
+           value#round({precision}):set() as values_list,
+           timestamp:min() as start,
+           timestamp:max() as end,
+          every {sampling_frequency} seconds over last {window_duration} seconds
+        => @filter(num_changes >= {max_threshold})
+        => @throttle 1, {window_duration} seconds
+    """
+    logging.debug(f'query is "{query}"')
+
+    return query
+
+
+def start(name, settings, helpers=None, task_id=None):
+    process_name = f"{name} - pretest"
+
+    if task_id:
+        action = Action.continue_task(task_id=task_id)
+    else:
+        action = start_action(action_type='flowrate_monitor')
+
+    with action.context():
         logging.info("{}: Flowrate monitor started".format(process_name))
-        setproctitle('DDA: Flowrate monitor')
-        session = requests.Session()
-        accumulator = []
+        setproctitle('DDA: Flowrate monitor "{}"'.format(process_name))
 
-        url = process_settings['request']['url']
-        interval = process_settings['request']['interval']
+        functions_map = {
+            'send_message': partial(
+                monitors.get_function('send_message', helpers),
+                extra_settings=settings,
+            ),
+            'create_annotation': partial(
+                monitors.get_function('create_annotation', helpers),
+                extra_settings=settings,
+            ),
+            'run_query': monitors.get_function(
+                'run_query', helpers
+            ),
+        }
 
-        send_message = partial(
-            monitors.get_function('send_message', helpers),
-            extra_settings=process_settings,
+        monitor_settings = settings.get('monitor', {})
+        window_duration = monitor_settings['window_duration']
+
+        results_process, results_queue = functions_map.get('run_query')(
+            build_query(settings),
+            span=f"last {window_duration} seconds",
+            realtime=True,
         )
 
-        iterations = 0
-        while True:
-            try:
-                r = session.get(url)
-                r.raise_for_status()
+        def process_events(accumulator):
+            check_rate(
+                process_name,
+                accumulator,
+                settings,
+                functions_map.get('send_message'),
+            )
 
-                flowrate_data = r.json()
+        try:
+            monitors.handle_events(
+                process_events,
+                results_queue,
+                settings,
+                timeout=read_timeout
+            )
+        except queue.Empty:
+            start(name, settings, helpers=helpers, task_id=task_id)
 
-                accumulator = check_rate(
-                    process_name,
-                    flowrate_data,
-                    accumulator,
-                    process_settings,
-                    send_message
-                )
-                logging.debug("{}: Request {} successful".format(
-                    process_name, iterations
-                ))
-
-            except Exception as e:
-                logging.error(
-                    "{}: Error processing events during request {}, {}<{}>".format(
-                        process_name, iterations, e, type(e)
-                    )
-                )
-
-            loop.await_next_cycle(interval, process_name)
-            iterations += 1
+    action.finish()
 
     return
