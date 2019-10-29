26444ce6a734bdd97b9682871cfc74a8b31eb007
Eric Chiesse <eric.chiesse@intelie.com.br>
2019-10-29 18:34:48 -0300

    Getting the well_id from the pipes query result.

--- src/main/chatbot_modules/adapters/torque_and_drag.py
+++ src/main/chatbot_modules/adapters/torque_and_drag.py
@@ -20,31 +20,46 @@ from ..constants import (
 tnd_query_template = """
 -- custom functions
 def @custom_batch():
-    list(*) every ((*->timestamp# - (*->timestamp#:prev ?? *->timestamp#))>10) before => @for => @yield;
+    list(*) every (
+      (*->timestamp# - (*->timestamp#:prev ?? *->timestamp#)) > 10
+    ) before
+    => @for
+    => @yield;
+
 def convert_to_custom_unit(mnemonic, uom, value):
     curve_unit_force_convert(value#, uom, mnemonic:decode("DBTM", "m","HKLA", "N", "MFIA", "m3/s"));
 
 -- getting tripping in data
 va007_trip_out .timestamp:adjusted_index_timestamp mnemonic!:(WOBA|DMEA|RPMA|ROPA|DBTM|MFIA|BPOS|HKLA)
--- @@lookup.rig_event_type .timestamp:adjusted_index_timestamp mnemonic!:(WOBA|DMEA|RPMA|ROPA|DBTM|MFIA|BPOS|HKLA)
 
--- => @meta @@lookup.span_by_rig_name[0]?? @@userspan as span
 -- making batch of data and calculating opmode
 => @custom_batch
+=> expand
+  normalized_operating_mode(
+    timestamp#, mnemonic, value#, uom,
+    'WOBA', 'DMEA', 'RPMA', 'ROPA', 'DBTM', 'MFIA', 'BPOS', 'HKLA'
+  ),
+  map(mnemonic, convert_to_custom_unit(mnemonic, uom, value)) as values,
+  lookup.rig_event_type_well_id.get(__type) as well_id
+  every batch
 
-=> expand normalized_operating_mode(timestamp#, mnemonic, value#,  uom, 'WOBA', 'DMEA', 'RPMA', 'ROPA',
-'DBTM', 'MFIA', 'BPOS', 'HKLA'), map(mnemonic, convert_to_custom_unit(mnemonic, uom, value)) as values every batch
 -- filtering by opmode
-=> @filter operating_mode != null && operating_mode != 'Connection' && operating_mode != 'Drilling Connection' && operating_mode != 'Tripping Connection'
+=> @filter
+  operating_mode != null
+  && operating_mode != 'Connection'
+  && operating_mode != 'Drilling Connection'
+  && operating_mode != 'Tripping Connection'
+
 -- filtering by bit depth value
 => @filter values['DBTM'] >= {min_depth}
 => @filter values['DBTM'] < {max_depth}
+
 -- filtering by low hook load values
 => @filter values['HKLA'] > 900000
--- printing data
-=> newmap('flowRate', values['MFIA'], 'hookLoad', values['HKLA'], 'bitDepth', values['DBTM'])
-=> @yield
 
+-- printing data
+=> newmap('flowRate', values['MFIA'], 'hookLoad', values['HKLA'], 'bitDepth', values['DBTM'], 'wellId', well_id)
+=> @yield
 """
 
 
@@ -71,36 +86,37 @@ class TorqueAndDragAdapter(WithAssetAdapter, BaseBayesAdapter):
         confidence = self.get_confidence(statement)
         params = self.extract_calibration_params(statement.text)
         if not params:
-            # NOTE: Não seria melhor fazer este tratamento em `can_process`?
             response = Statement("Sorry, I can't read the calibration parameters from your message")
             response.confidence = confidence
             return response
 
         points = self.retrieve_regression_points(params)
         # TODO: [ECS]: Parâmetros hardcoded abaixo, tratar: <<<<<
-        well_id = 6
-        travelling_block_weight = 900000
-        calibration_result = self.request_calibration(well_id, travelling_block_weight, points)
+        well_id = points[0]['wellId']
+
+        calibration_result = self.request_calibration(well_id, points)
 
         confidence = 1
-        response = self.build_response(calibration_result, confidence)
+        response = self.build_response(confidence, {
+            'wellId': well_id,
+            'calibration_result': calibration_result,
+        })
         return response
 
     def can_process(self, statement):
-        # NOTE: Transformei `_is_our_message` neste método, para que o chatterbot
-        # nem tente usar este adapter para processar mensagens que não são endereçadas a ele
         keywords = ['torque', 'drag']
         found_keywords = [word for word in statement.text.lower().split() if word in keywords]
         has_required_terms = sorted(found_keywords) == sorted(keywords)
         return has_required_terms and super().can_process(statement)
 
-    def build_response(self, calibration_result, confidence):
+    def build_response(self, confidence, context):
         message = f"""
-        Calibration Results:
-        -  Regression method: {calibration_result["calibrationMethod"]}
-        -  Travelling Block Weight: {calibration_result["travellingBlockWeight"]}
-        -  Pipes Weight Multiplier: {calibration_result["pipesWeightMultiplier"]}
-        """
+Calibration Results:
+-  Well ID: {context["wellId"]}
+-  Regression method: {context["calibration_result"]["calibrationMethod"]}
+-  Travelling Block Weight: {context["calibration_result"]["travellingBlockWeight"]}
+-  Pipes Weight Multiplier: {context["calibration_result"]["pipesWeightMultiplier"]}
+"""
         response = Statement(message)
         response.confidence = confidence
         return response
@@ -120,50 +136,49 @@ class TorqueAndDragAdapter(WithAssetAdapter, BaseBayesAdapter):
         return ret
 
     def run_query(self, query_str, realtime=False, span=None, callback=None):
-        # NOTE: Este `start_action` é redundante, `query_runner` já irá fazê-lo
-        with start_action(action_type=self.state_key, pipes_query=query_str):
-            results_process, results_queue = self.query_runner(
-                query_str,
-                realtime=realtime,
-                span=span,
-            )
+        results_process, results_queue = self.query_runner(
+            query_str,
+            realtime=realtime,
+            span=span,
+        )
 
-            result = []
-            while True:
-                try:
-                    event = results_queue.get(timeout=self.query_timeout)
-                    event_data = event.get('data', {})
-                    event_type = event_data.get('type')
-                    if event_type == EVENT_TYPE_EVENT:
-                        result.extend(event_data.get('content', []))
+        result = []
+        while True:
+            try:
+                event = results_queue.get(timeout=self.query_timeout)
+                event_data = event.get('data', {})
+                event_type = event_data.get('type')
+                if event_type == EVENT_TYPE_EVENT:
+                    result.extend(event_data.get('content', []))
 
-                    if event_type == EVENT_TYPE_DESTROY:
-                        break
+                if event_type == EVENT_TYPE_DESTROY:
+                    break
 
-                except queue.Empty as e:
-                    logging.exception(e)
+            except queue.Empty as e:
+                logging.exception(e)
 
-            results_process.join(1)
+        results_process.join(1)
 
-            if callback is not None:
-                return callback(result)
-            return result
+        if callback is not None:
+            return callback(result)
+        return result
 
     def build_calibration_data(self, well_id, travelling_block_weight, points):
         return {
-            "wellId": f"{well_id}",  # NOTE: Precisa mesmo forçar que seja uma string?
+            "wellId": well_id,
             "travellingBlockWeight": travelling_block_weight,
             "saveResult": "true",
             "calibrationMethod": "LINEAR_REGRESSION",
             "points": points
         }
 
-    def request_calibration(self, well_id, travelling_block_weight, points):
+    def request_calibration(self, well_id, points):
         service_path = '/services/plugin-og-model-torquendrag/calibrate/'
         url = f'{self.live_host}{service_path}'
 
         s = requests.Session()
         s.auth = (self.username, self.password)
+        travelling_block_weight = 0 # This value is ignored for LINEAR_REGRESSION (the only supported right now)
         calibration_data = self.build_calibration_data(well_id, travelling_block_weight, points)
         response = s.post(
             url,
@@ -183,12 +198,10 @@ class TorqueAndDragAdapter(WithAssetAdapter, BaseBayesAdapter):
 
     def retrieve_regression_points(self, params):
         points = []
-        # NOTE: Não seria melhor usar uma f-string na definição do template e só popula-la aqui?
-        pipes_query = (
-            tnd_query_template
-            .replace('{min_depth}', params['min_depth'])
-            .replace('{max_depth}', params['max_depth'])
-        )
+        pipes_query = (tnd_query_template.format(
+            min_depth = params['min_depth'],
+            max_depth = params['max_depth'],
+        ))
 
         start_time = params['start_time']
         end_time = params['end_time']
