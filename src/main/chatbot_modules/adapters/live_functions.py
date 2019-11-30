065a4e8aa473a2a02450e122c55fe0b8f8bc57d0
Eric Chiesse <eric.chiesse@intelie.com.br>
2019-11-30 00:26:00 -0300

    Adapted "AutoAnalysisAdapter" to perform actual analysis only after the response has been selected.

--- src/main/chatbot_modules/adapters/live_functions.py
+++ src/main/chatbot_modules/adapters/live_functions.py
@@ -5,6 +5,7 @@ from uuid import uuid4
 from chatterbot.conversation import Statement
 from eliot import start_action
 
+from dda.chatbot.actions import ShowTextAction, CallbackAction
 from live_client.assets import run_analysis
 from live_client.events.constants import TIMESTAMP_KEY
 from live_client.utils.timestamp import get_timestamp
@@ -95,7 +96,7 @@ class AutoAnalysisAdapter(BaseBayesAdapter, NLPAdapter, WithAssetAdapter):
 
         return value
 
-    def run_analysis(self, asset, curve, begin=None, duration=30000, confidence=0):
+    def run_analysis(self, asset, curve, begin=None, duration=30000):
         if begin is None:
             begin = get_timestamp() - duration
 
@@ -122,14 +123,13 @@ class AutoAnalysisAdapter(BaseBayesAdapter, NLPAdapter, WithAssetAdapter):
                 self.annotator("{} for {}".format(self.state_key, curve), analysis_results)
 
             response_text = "Analysis of curve {} finished".format(curve)
-            confidence = 1  # Otherwise another answer might be chosen
 
         else:
             response_text = "Analysis of curve {} returned no data".format(curve)
 
-        return response_text, confidence
+        return response_text
 
-    def run_query(self, asset, curve, index_value, confidence=0):
+    def run_query(self, asset, curve, index_value):
         selected_asset = self.get_selected_asset()
         if selected_asset:
             asset_config = selected_asset.get("asset_config", {})
@@ -155,11 +155,10 @@ class AutoAnalysisAdapter(BaseBayesAdapter, NLPAdapter, WithAssetAdapter):
                     asset=asset,
                     curve=curve,
                     index_value=index_value,
-                    confidence=confidence,
                 ),
             )
 
-    def prepare_analysis(self, content, asset=None, curve=None, index_value=None, confidence=0):
+    def prepare_analysis(self, content, asset=None, curve=None, index_value=None):
         if not content:
             response_text = "No information about {curve} at {index_curve} {index_value}".format(
                 curve=curve, index_curve=self.index_curve, index_value=index_value
@@ -170,13 +169,11 @@ class AutoAnalysisAdapter(BaseBayesAdapter, NLPAdapter, WithAssetAdapter):
                 timestamp = int(item.get(TIMESTAMP_KEY, 0)) or None
 
                 with start_action(action_type=self.state_key, curve=curve, begin=timestamp):
-                    response_text, confidence = self.run_analysis(
-                        asset, curve, begin=timestamp, confidence=confidence
-                    )
+                    response_text = self.run_analysis(asset, curve, begin=timestamp)
 
-        return response_text, confidence
+        return response_text
 
-    def process_direct_analysis(self, statement, selected_asset, confidence=0, begin=None):
+    def process_direct_analysis(self, statement, selected_asset, begin=None):
         selected_curves = self.find_selected_curves(statement)
         num_selected_curves = len(selected_curves)
 
@@ -186,9 +183,7 @@ class AutoAnalysisAdapter(BaseBayesAdapter, NLPAdapter, WithAssetAdapter):
             ##
             # Iniciar analise
             with start_action(action_type=self.state_key, curve=selected_curve):
-                response_text, confidence = self.run_analysis(
-                    selected_asset, selected_curve, confidence=confidence
-                )
+                response_text = self.run_analysis(selected_asset, selected_curve)
 
         elif num_selected_curves == 0:
             response_text = "I didn't get the curve name. Can you repeat please?"
@@ -198,9 +193,9 @@ class AutoAnalysisAdapter(BaseBayesAdapter, NLPAdapter, WithAssetAdapter):
                 ITEM_PREFIX, ITEM_PREFIX.join(selected_curves)
             )
 
-        return response_text, confidence
+        return response_text
 
-    def process_indexed_analysis(self, statement, selected_asset, confidence=0):
+    def process_indexed_analysis(self, statement, selected_asset):
         selected_curves = self.find_selected_curves(statement)
         num_selected_curves = len(selected_curves)
         selected_value = self.find_index_value(statement)
@@ -215,41 +210,30 @@ class AutoAnalysisAdapter(BaseBayesAdapter, NLPAdapter, WithAssetAdapter):
             selected_curve = selected_curves[0]
 
             with start_action(action_type=self.state_key, curve=selected_curve):
-                response_text, confidence = self.run_query(
-                    selected_asset, selected_curve, selected_value, confidence=confidence
-                )
+                response_text = self.run_query(selected_asset, selected_curve, selected_value)
 
         else:
             response_text = "I'm sorry, which of the curves you chose?{}{}".format(
                 ITEM_PREFIX, ITEM_PREFIX.join(selected_curves)
             )
 
-        return response_text, confidence
+        return response_text
 
-    def process(self, statement, additional_response_selection_parameters=None): # !! FIXME: Move Side Effects outside !! <<<<<
+    def process(self, statement, additional_response_selection_parameters=None):
         confidence = self.get_confidence(statement)
 
         if confidence > self.confidence_threshold:
-            self.load_state()
+            self.load_state() # <<<<< [ECS]: Essa chamada é necessária para chamar 'get_selected_asset' ? Se for ver um modo melhor de garantir esse passo.
             selected_asset = self.get_selected_asset()
-
-            if selected_asset is None:
+            if selected_asset == {}:
                 response_text = "No asset selected. Please select an asset first."
             else:
-                mentioned_curves = self.list_mentioned_curves(statement)
-                has_index_mention = (len(mentioned_curves) > 1) and (
-                    self.index_curve in mentioned_curves
+                return CallbackAction(
+                    self.perform_analysis,
+                    confidence = 1,
+                    statement = statement,
+                    selected_asset = selected_asset
                 )
-
-                if has_index_mention:
-                    response_text, confidence = self.process_indexed_analysis(
-                        statement, selected_asset, confidence=confidence
-                    )
-
-                else:
-                    response_text, confidence = self.process_direct_analysis(
-                        statement, selected_asset, confidence=confidence
-                    )
 
             response = Statement(text=response_text)
             response.confidence = confidence
@@ -257,3 +241,13 @@ class AutoAnalysisAdapter(BaseBayesAdapter, NLPAdapter, WithAssetAdapter):
             response = None
 
         return response
+
+    def perform_analysis(self, statement, selected_asset):
+        mentioned_curves = self.list_mentioned_curves(statement)
+        has_index_mention = (len(mentioned_curves) > 1) and (
+            self.index_curve in mentioned_curves
+        )
+
+        processor = self.process_indexed_analysis if has_index_mention else self.process_direct_analysis
+        response_text = processor(statement, selected_asset)
+        return response_text
