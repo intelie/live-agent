40241bfb36913eb6e5936cdccc8d80647d0e3e42
Vitor <vitor.mazzi@intelie.com.br>
2020-03-04 15:25:00 -0300

    `README` updated for the current structure

--- setup.py
+++ setup.py
@@ -22,8 +22,13 @@ def get_readme():
     return ""
 
 
-def install_requires():
-    requirements = read_file("requirements.txt")
+def install_requires(module=None):
+    if module:
+        filepath = f"./live_agent/modules/{module}/"
+    else:
+        filepath = ""
+
+    requirements = read_file(f"{filepath}requirements.txt")
     return requirements
 
 
@@ -40,16 +45,10 @@ setup(
     author_email="vitor.mazzi@intelie.com.br",
     install_requires=install_requires(),
     extras_require={
-        "chatbot": [
-            "ChatterBot==1.0.5",
-            "chatterbot-corpus==1.2.0",
-            "Jinja2==2.10.1",
-            "pytz==2019.2",
-            "python-dateutil>=2.7,<2.8",
-            "PyYAML>=3.12,<4.0",
-        ],
-        "las": ["lasio==0.23", "pandas==0.24.2", "scikit-learn>=0.20"],
-        "mdt": ["scikit-learn>=0.20"],
+        "chatbot": install_requires(module="chatbot"),
+        "las": install_requires(module="las"),
+        "mdt": install_requires(module="mdt"),
+        "torque_and_drag": [],
     },
     zip_safe=False,
     python_requires=">=3.6",
