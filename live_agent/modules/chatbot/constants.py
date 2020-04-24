2a04c3cca2388d92e13d333642b4508c7227029e
Vitor <vitor.mazzi@intelie.com.br>
2020-04-24 18:29:32 -0300

    Removing some formatting from the bot's features

--- live_agent/modules/chatbot/constants.py
+++ live_agent/modules/chatbot/constants.py
@@ -6,6 +6,6 @@ SELECTED_ASSET_VARIABLE_NAME = "selected-asset"
 FEATURES_DESCRIPTION_TEMPLATE = """My features are:
 {% for feature_data in features %}
   *{{ feature_data.description }}*
-  Try: _"{{ bot_name }}, {{ feature_data.usage_example }}"_
+  Try: "{{ bot_name }}, {{ feature_data.usage_example }}"
 {% endfor %}
 """
